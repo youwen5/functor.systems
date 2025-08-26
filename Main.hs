8e8d3adc924d4468bdd638ca5c0158377fa0e790
Youwen Wu <youwenw@gmail.com>
2025-08-26 11:34:13 -0700

    feat: basic miso build with WASM

---
+++ Main.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+----------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+
+----------------------------------------------------------------------------
+module Main where
+
+----------------------------------------------------------------------------
+import Miso
+import qualified Miso.Html as H
+import qualified Miso.Html.Property as P
+import Miso.Lens
+
+----------------------------------------------------------------------------
+
+-- | Component model state
+data Model where
+    Model :: {_counter :: Int} -> Model
+    deriving (Show, Eq)
+
+----------------------------------------------------------------------------
+counter :: Lens Model Int
+counter = lens _counter $ \record field -> record{_counter = field}
+
+----------------------------------------------------------------------------
+
+-- | Sum type for App events
+data Action
+    = AddOne
+    | SubtractOne
+    | SayHelloWorld
+    deriving (Show, Eq)
+
+----------------------------------------------------------------------------
+
+-- | Entry point for a miso application
+main :: IO ()
+main = run (startApp app)
+
+----------------------------------------------------------------------------
+
+-- | WASM export, required when compiling w/ the WASM backend.
+#ifdef WASM
+foreign export javascript "hs_start" main :: IO ()
+#endif
+----------------------------------------------------------------------------
+
+-- | `component` takes as arguments the initial model, update function, view function
+app :: App Model Action
+app = component emptyModel updateModel viewModel
+
+----------------------------------------------------------------------------
+
+-- | Empty application state
+emptyModel :: Model
+emptyModel = Model 0
+
+----------------------------------------------------------------------------
+
+-- | Updates model, optionally introduces side effects
+updateModel :: Action -> Transition Model Action
+updateModel = \case
+    AddOne -> counter += 1
+    SubtractOne -> counter -= 1
+    SayHelloWorld -> io_ $ do
+        alert "Hello World"
+        consoleLog "Hello World"
+
+----------------------------------------------------------------------------
+
+-- | Constructs a virtual DOM from a model
+viewModel :: Model -> View Model Action
+viewModel x =
+    H.div_
+        [ P.className "counter"
+        ]
+        [ H.button_ [H.onClick AddOne] [text "+"]
+        , text $ ms (x ^. counter)
+        , H.button_ [H.onClick SubtractOne] [text "-"]
+        , H.br_ []
+        , H.button_ [H.onClick SayHelloWorld] [text "Alert Hello World!"]
+        ]
+
+----------------------------------------------------------------------------
---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "The website for functor.systems, developed using Miso, a Haskell web framework.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    miso.url = "github:dmjio/miso/master";
+    miso.flake = false;
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      miso,
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.linux;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          ghcjs = pkgs.pkgsCross.ghcjs.buildPackages;
+        in
+        {
+          default = self.packages.${system}.website-unwrapped;
+          miso = ghcjs.haskell.ghcjs.haskellPackages.callPackage "miso" miso { };
+          website-unwrapped = ghcjs.haskellPackages.callCabal2nix "website" ./. {
+            inherit (self.packages.${system}) miso;
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          ghcjs = pkgs.pkgsCross.ghcjs.buildPackages;
+        in
+        {
+          default = ghcjs.haskellPackages.shellFor {
+            packages = hsPkgs: [
+              hsPkgs.distribution-nixpkgs
+              self.packages.${system}.default
+            ];
+
+            withHoogle = true;
+
+            nativeBuildInputs =
+              (with pkgs; [
+                cabal-install
+                haskellPackages.cabal-gild
+                haskellPackages.haskell-language-server
+                emscripten
+                nodejs
+              ])
+              ++ [ ghcjs.ghc ];
+          };
+        }
+      );
+    };
+}
