b6f7dd20b763d6ce9cab81a5ede5381496dea011
Anthony Wang <a@unnamed.website>
2026-08-25 23:12:10 -0500

    Remove gigantic SVG favicon
    It's at least 50% of the total size of this site

--- Site/Theme.lean
+++ Site/Theme.lean
@@ -27,7 +27,6 @@ def theme : Theme := { Theme.default with
             content="a homotopy-coherent free software collective."
           />
           <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
-          <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
 
           <link rel="stylesheet" href="latex.css" />
